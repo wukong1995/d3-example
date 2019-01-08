1d15805892292b5c8abfb4d98b0d83b206569767
Peipei Wong <wangpp1995@gmail.com>
2019-01-08 14:47:20 +0800

    Add zoom to svg

--- src/scripts/shared/getSvg.js
+++ src/scripts/shared/getSvg.js
@@ -3,15 +3,14 @@ import * as d3 from 'd3v4';
 export default () => {
   const svg = d3.select('svg');
 
-  // svg drag and zoom
-  // svg.call(d3.behavior.drag()
-  //   .on('drag', dragmove));
-  // svg.call(d3.behavior.zoom()
-  //   .x(xScale)
-  //   .y(yScale)
-  //   .scaleExtent([1, 6])
-  //   .on('zoom', doZoom));
+  // zoom
+  svg.call(d3.zoom()
+    .scaleExtent([.5, 8])
+    .on('zoom', zoom));
 
-  // svg drag and zoom
+  function zoom() {
+    d3.selectAll('g').attr('transform', d3.event.transform);
+  }
+
   return svg;
 };
